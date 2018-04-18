7df74a01f288443d70614d242670cb51e613d1f5
milica6695 <milica6695@gmail.com>
2018-04-18 04:39:11 +0200

    fan zona

--- bioskopi/src/main/resources/static/js/fanzone.js
+++ bioskopi/src/main/resources/static/js/fanzone.js
@@ -1,12 +1,11 @@
-function props(){
+function thematicprops(){
 	$(".welcome").empty();
-	console.log("kliknuo");
 	$.ajax({
 		  method : 'GET',
 		  url : "/thematic_props/get_all_thematic_props",
 		  success : function(data){
-			  console.log("uspjesno!");
-			  podijeliOglase(data);
+			  console.log("uspelo!");
+			  podeliOglase(data);
 		  },
 		  error: function(){
 			  console.log("neuspesno");
@@ -15,15 +14,15 @@ function props(){
 	});
 }
 
-function podijeliOglase(data) {
-	console.log("usao u podjelu");
+function podeliOglase(data) {
+	console.log("usao u podelu");
 	var list = data == null ? [] : (data instanceof Array ? data : [ data ]);
 	$.each(list, function(index, oglas) {
 	console.log(oglas.name);
 	if(oglas.tptype == "NEW") {
 	$(".welcome").append(`<div class="panel panel-default form-group" id="pojedinacni">
 	 			<div id="divNaziv" class="panel-heading"><label id="odmakniMe"><b>`+oglas.name+`</b><br><label>
-	 			<label id="odmakniMe" style="color:green"><b>`+oglas.tptype+`</b><label></div>
+	 			<label id="odmakniMe" style="color:#A16073"><b>`+oglas.tptype+`</b><label></div>
 	 			<div id="divOpis" class="panel-body"><textarea readonly id="divOpis2" class="form-control" rows="6">`+oglas.description+`</textarea></div>
 	 			<div id="batoni">
    					<button type="button" class="btn btn-success rekvizitButtoni" name="ponudi`+oglas.name+`">Make an offer</button>
@@ -33,7 +32,7 @@ function podijeliOglase(data) {
 	} else {
 		$(".welcome").append(`<div class="panel panel-default form-group" id="pojedinacni">
 	 			<div id="divNaziv" class="panel-heading"><label id="odmakniMe"><b>`+oglas.name+`</b><br><label>
-	 			<label id="odmakniMe" style="color:yellow"><b>`+oglas.tptype+`</b><label></div>
+	 			<label id="odmakniMe" style="color:#A16073"><b>`+oglas.tptype+`</b><label></div>
 	 			<div id="divOpis" class="panel-body"><textarea readonly id="divOpis2" class="form-control" rows="6">`+oglas.description+`</textarea></div>
 	 			<div id="batoni">
    					<button type="button" class="btn btn-success rekvizitButtoni" name="ponudi`+oglas.name+`">Make an offer</button>
@@ -43,6 +42,42 @@ function podijeliOglase(data) {
 	}
 		});
 }
+
+
+function izmjeniOglas(obj) {
+	var pokusaj = obj.name;
+	console.log("pokusaj na "+pokusaj);
+	var konacan = pokusaj.split("izmjeni")[1];
+	$(document).on("click",obj,function(e) {
+		e.preventDefault();		
+		$.ajax({
+			method : 'GET',
+			url : "/thematic_props",
+			success : function(data){
+				console.log("uspjesno!");
+				nadjiOglas(data,konacan);
+			},
+			error: function(){
+				console.log("neuspesno");
+			}
+		});
+		
+	});
+}
+
+function nadjiOglas(data,kod) {
+	var list = data == null ? [] : (data instanceof Array ? data : [ data ]);
+	$.each(list, function(index, oglas) {
+	console.log(oglas.name);
+	if(oglas.id == kod) {
+		sessionStorage.setItem('mijenjanje',JSON.stringify(oglas));
+		sessionStorage.setItem('tipRekvizit',"edit");
+		window.location.href = 'thematicProps.html';
+	}
+	});
+}
+
+
 
 function izbrisiOglas(obj){
 	var pokusaj = obj.name;
@@ -56,8 +91,8 @@ function izbrisiOglas(obj){
 			method : 'GET',
 			url : "/thematic_props/delete/"+konacan,
 			success : function(data){
-				console.log("uspjesno!");
-				props();
+				console.log("uspesno!");
+				thematicprops();
 			},
 			error: function(){
 				console.log("neuspesno");
