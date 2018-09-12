45014fe99cfb7dd9ee77dc4e0ccf37d61b6fe60a
milica6695 <milica6695@gmail.com>
2018-09-13 01:11:25 +0200

    add method setRole

--- bioskopi/src/main/java/rs/ac/uns/ftn/informatika/jpa/domain/DTOs/PrihodDTO.java
+++ bioskopi/src/main/java/rs/ac/uns/ftn/informatika/jpa/domain/DTOs/PrihodDTO.java
@@ -55,5 +55,37 @@ public class PrihodDTO {
 		return "PrihodDTO [idPozBio=" + idPozBio + ", datumOd=" + datumOd + ", datumDo=" + datumDo + ", mode=" + mode
 				+ "]";
 	}
+
+
+		
+	@RequestMapping(value = "set_role/{user_id}/{role}",
+			method = RequestMethod.POST,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public boolean setRole(@PathVariable Long user_id, @PathVariable Role role) {
+		if (user_id == SessionService.getCurrentlyLoggedUser().getId())
+			return false; //ne mozes sam sebi menjati role
+		return this.userService.setRole(user_id, role);
+	}
+
+		@Autowired
+	private ReservationService reservationService;
 	
+	@RequestMapping(value = "add_new_reservation",
+			method = RequestMethod.POST,
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public boolean addNewReservation(@RequestBody Reservation reservation) {
+		return this.reservationService.makeNewReservation(reservation);
+	}
+	
+	@RequestMapping(value = "refuse_invitation/{reservation_id}",
+			method = RequestMethod.POST,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public boolean addNewHallForTheater(@PathVariable Long reservation_id) {
+		return this.reservationService.cancelReservedSeat(reservation_id);
+	}
+		
 }
